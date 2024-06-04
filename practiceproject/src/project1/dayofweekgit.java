acbbc66833e7836950eb7746ebd43324c5f35acc
Admin <Admin@DESKTOP-GB66R0S>
2024-06-04 19:52:03 +0530

    first commit for dayofweek project

---
+++ practiceproject/src/project1/dayofweekgit.java
@@ -0,0 +1,29 @@
+package project1;
+
+import java.time.DayOfWeek;
+import java.time.LocalDate;
+import java.util.Scanner;
+
+public class dayofweekgit {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner scan=new Scanner(System.in);
+		System.out.println("Enter the date (yyyy-mm-dd)");
+		String inputdate=scan.nextLine();	
+		LocalDate date=LocalDate.parse(inputdate);
+		DayOfWeek day=date.getDayOfWeek();
+		System.out.println(day);
+		if(isweekend(day)){
+			 System.out.println(day + " is a happyweekend.");
+		}else {
+			 System.out.println(day + " is a weekday.");
+		}
+				
+	}
+	
+	 public static boolean isweekend(DayOfWeek ans) {
+		 return ans == DayOfWeek.SATURDAY || ans == DayOfWeek.SUNDAY;
+	 }
+	 
+}
